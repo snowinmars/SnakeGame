8c2c3353207ab3314de81a838edae8cd696c02e2
snowonmars <stuff4work@yandex.ru>
2016-07-04 20:11:12 +0300

    PL init

---
+++ SnackGame/Configuration/Comfig.cs
@@ -0,0 +1,6 @@
+﻿namespace Configuration
+{
+    public static class Comfig
+    {
+    }
+}
---
+++ SnackGame/SnackGame.PL.Monogame/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SnackGame.PL.Monogame
+{
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+            using (var game = new SnakeGame())
+                game.Run();
+        }
+    }
+}
